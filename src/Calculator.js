11d30d15a93bb9c44c15c99b799ba0b140e87484
Ruben Cedeno <rubencedeno@rubens-mbp.myfiosgateway.com>
2020-12-14 21:22:00 -0500

    [Deliverable]

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,7 +1,62 @@
 import React, { useState } from 'react'
 
 const Calculator = props => {
-    // Declare state variables
+    
+    const [num1, setNum1] = useState("")
+    const [num2, setNum2] = useState("")
+    const [operator, setOperator] = useState("")
+    const [value, setValue] = useState("")
+    const [nope, setNope] = useState("")
+
+    const handleClear= () => {
+        setNum1("")
+        setNum2("")
+        setOperator("")
+        setValue("")
+        setNope("")
+    }
+
+    const handleOperation=(e) => {
+
+        
+        if (e === "x" || e === "-" || e === "/" || e === "+") {
+            if (operator.length === 1) {
+            setNope(`Nope, you already click ${e}`)
+            return
+            }
+            setOperator(`${operator}${e}`)
+            setNope("")
+        } else if (operator === "" && num2 === "") {
+            if (e === "0" && num1 === "") {
+                setNope("First number can't be a 0!")
+                return
+            }
+             setNum1(`${num1}${e}`)
+             setNope("")
+        } else {
+            if (num1 !== "" && num2 !== "" && operator !== "" && e === "=") {
+                if (operator === "+") {
+                    setValue(parseInt(num1) + parseInt(num2))
+                    setNope("")
+                } else if (operator === "-") {
+                    setValue(parseInt(num1) - parseInt(num2))
+                    setNope("")
+                } else if (operator === "/") {
+                    setValue(parseInt(num1) / parseInt(num2))
+                    setNope("")
+                } else {
+                    setValue(parseInt(num1) * parseInt(num2))
+                    setNope("")
+                }
+            }
+            if (e === "0" && num2 === "") {
+                setNope("First number can't be a 0!")
+                return
+            }
+            setNum2(`${num2}${e}`)
+            setNope("")
+        }
+    }
 
 
     return (
@@ -9,37 +64,38 @@ const Calculator = props => {
             <h1>React Calculator</h1>
             <div className="calc-container">
                 <p>Values: </p>
-                <div className="answer-box">TBD</div>
+                <div className="answer-box">{num1}{operator}{num2}{value ? `${value}` : ""} </div>
                 <div className="calc-row">
-                    <button className="calc-button calc-button-top">AC</button>
-                    <button className="calc-button calc-button-top">+/-</button>
-                    <button className="calc-button calc-button-top">%</button>
-                    <button className="calc-button calc-button-op">/</button>
+                    <button onClick = {()=> handleClear()} className="calc-button calc-button-top">AC</button>
+                    <button onClick = {()=> handleOperation("+")}  className="calc-button calc-button-top">+/-</button>
+                    <button onClick = {()=> handleOperation("%")}  className="calc-button calc-button-top">%</button>
+                    <button onClick = {()=> handleOperation("/")}  className="calc-button calc-button-op">/</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">7</button>
-                    <button className="calc-button">8</button>
-                    <button className="calc-button">9</button>
-                    <button className="calc-button calc-button-op">x</button>
+                    <button onClick = {()=> handleOperation("7")} className="calc-button">7</button>
+                    <button onClick = {()=> handleOperation("8")} className="calc-button">8</button>
+                    <button onClick = {()=> handleOperation("9")} className="calc-button">9</button>
+                    <button onClick = {()=> handleOperation("x")} className="calc-button calc-button-op">x</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">4</button>
-                    <button className="calc-button">5</button>
-                    <button className="calc-button">6</button>
-                    <button className="calc-button calc-button-op">-</button>
+                    <button onClick = {()=> handleOperation("4")} className="calc-button">4</button>
+                    <button onClick = {()=> handleOperation("5")} className="calc-button">5</button>
+                    <button onClick = {()=> handleOperation("6")} className="calc-button">6</button>
+                    <button onClick = {()=> handleOperation("-")} className="calc-button calc-button-op">-</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">1</button>
-                    <button className="calc-button">2</button>
-                    <button className="calc-button">3</button>
-                    <button className="calc-button calc-button-op">+</button>
+                    <button onClick = {()=> handleOperation("1")} className="calc-button">1</button>
+                    <button onClick = {()=> handleOperation("2")} className="calc-button">2</button>
+                    <button onClick = {()=> handleOperation("3")} className="calc-button">3</button>
+                    <button onClick = {()=> handleOperation("+")} className="calc-button calc-button-op">+</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button width-2">0</button>
+                    <button onClick = {()=> handleOperation("0")}  className="calc-button width-2">0</button>
                     <button className="calc-button">.</button>
-                    <button className="calc-button calc-button-op">=</button>
+                    <button onClick = {()=> handleOperation("=")}  className="calc-button calc-button-op">=</button>
                 </div>
             </div>
+            <h3>{nope}</h3>
         </div>
     )
 }
