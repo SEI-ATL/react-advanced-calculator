54de315c3a16a3146ebfecdc3a61adef078b4e0b
Angel's Work Station (Coding) <angelsworkstation@Angels-iMac.local>
2020-12-14 21:06:31 -0800

    [Calculator.js] semi working calculator. Bugs need fixing

--- src/Calculator.js
+++ src/Calculator.js
@@ -8,10 +8,16 @@ const Calculator = props => {
     const [num2, setNum2] = useState('');
     const [operator, setOperator] = useState('');
     const [total, setTotatl] = useState('');
+    const [error, setError] = useState('');
 
     const handleClick = (e) => {
         if (e === '+' || e === '-' || e === 'x' || e === '/') {
+            if (operator.length === 1) {
+                setError('Only one operator')
+                return
+            }
             setOperator(`${operator}${e}`)
+            setError('')
         } else if (operator === '' && num2 === '') {
             setNum1(`${num1}${e}`)
             } else {
@@ -20,7 +26,7 @@ const Calculator = props => {
                         setTotatl(parseInt(num1) + parseInt(num2))
                     } else if (operator === '-') {
                         setTotatl(parseInt(num1) - parseInt(num2))
-                    } else if (operaor === 'x') {
+                    } else if (operator === 'x') {
                         setTotatl(parseInt(num1) * parseInt(num2))
                     } else if (operator === '/') {
                         setTotatl(parseInt(num1) / parseInt(num2))
@@ -33,7 +39,7 @@ const Calculator = props => {
         
 
     // Added a handleClear function that will clear everything
-    handleClear = () => {
+    const handleClear = () => {
         setNum1('');
         setNum2('');
         setOperator('');
@@ -45,36 +51,35 @@ const Calculator = props => {
         <div className="container">
             <h1>React Calculator</h1>
             <div className="calc-container">
-                <p>Values: </p>
-                <div className="answer-box">TBD</div>
+                <div className="answer-box">{num1}{operator}{num2} {total ? `${total}` : ''}</div>
                 <div className="calc-row">
-                    <button className="calc-button calc-button-top">AC</button>
+                    <button onClick={() => handleClear()} className="calc-button calc-button-top">AC</button>
                     <button className="calc-button calc-button-top">+/-</button>
                     <button className="calc-button calc-button-top">%</button>
-                    <button className="calc-button calc-button-op">/</button>
+                    <button onClick={() => handleClick('/')}className="calc-button calc-button-op">/</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">7</button>
-                    <button className="calc-button">8</button>
-                    <button className="calc-button">9</button>
-                    <button className="calc-button calc-button-op">x</button>
+                    <button onClick={() => handleClick('7')}className="calc-button">7</button>
+                    <button onClick={() => handleClick('8')}className="calc-button">8</button>
+                    <button onClick={() => handleClick('9')}className="calc-button">9</button>
+                    <button onClick={() => handleClick('x')}className="calc-button calc-button-op">x</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">4</button>
-                    <button className="calc-button">5</button>
-                    <button className="calc-button">6</button>
-                    <button className="calc-button calc-button-op">-</button>
+                    <button onClick={() => handleClick('4')}className="calc-button">4</button>
+                    <button onClick={() => handleClick('5')}className="calc-button">5</button>
+                    <button onClick={() => handleClick('6')}className="calc-button">6</button>
+                    <button onClick={() => handleClick('-')}className="calc-button calc-button-op">-</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">1</button>
-                    <button className="calc-button">2</button>
-                    <button className="calc-button">3</button>
-                    <button className="calc-button calc-button-op">+</button>
+                    <button onClick={() => handleClick('1')}className="calc-button">1</button>
+                    <button onClick={() => handleClick('2')}className="calc-button">2</button>
+                    <button onClick={() => handleClick('3')}className="calc-button">3</button>
+                    <button onClick={() => handleClick('+')}className="calc-button calc-button-op">+</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button width-2">0</button>
+                    <button onClick={() => handleClick('0')}className="calc-button width-2">0</button>
                     <button className="calc-button">.</button>
-                    <button className="calc-button calc-button-op">=</button>
+                    <button onClick={() => handleClick('=')}className="calc-button calc-button-op">=</button>
                 </div>
             </div>
         </div>
