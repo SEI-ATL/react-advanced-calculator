c2ec13e754fb2757761686cf1544e4d370a965c8
delayedaa <delayedaa@gmail.com>
2020-12-14 22:10:13 -0600

    [Calculator] Add functionality for main operators

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,47 +1,133 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
-const Calculator = props => {
-    // Declare state variables
+const Calculator = (props) => {
+  // Declare state variables
+  const [storedNum, setStoredNum] = useState(0);
+  const [displayedNum, setDisplayedNum] = useState('0');
+  const [currentOperator, setCurrentOperator] = useState('');
+  const [calculateMode, setCalculateMode] = useState(false);
 
+  const handleClickNum = (num) => {
+    if ((displayedNum === '0' && num !== '.') || calculateMode) {
+      setStoredNum(Number(displayedNum));
+      setDisplayedNum(num);
+      setCalculateMode(false);
+      resetButtons();
+    } else {
+      setDisplayedNum(displayedNum + num);
+    }
+  };
 
-    return (
-        <div className="container">
-            <h1>React Calculator</h1>
-            <div className="calc-container">
-                <p>Values: </p>
-                <div className="answer-box">TBD</div>
-                <div className="calc-row">
-                    <button className="calc-button calc-button-top">AC</button>
-                    <button className="calc-button calc-button-top">+/-</button>
-                    <button className="calc-button calc-button-top">%</button>
-                    <button className="calc-button calc-button-op">/</button>
-                </div>
-                <div className="calc-row">
-                    <button className="calc-button">7</button>
-                    <button className="calc-button">8</button>
-                    <button className="calc-button">9</button>
-                    <button className="calc-button calc-button-op">x</button>
-                </div>
-                <div className="calc-row">
-                    <button className="calc-button">4</button>
-                    <button className="calc-button">5</button>
-                    <button className="calc-button">6</button>
-                    <button className="calc-button calc-button-op">-</button>
-                </div>
-                <div className="calc-row">
-                    <button className="calc-button">1</button>
-                    <button className="calc-button">2</button>
-                    <button className="calc-button">3</button>
-                    <button className="calc-button calc-button-op">+</button>
-                </div>
-                <div className="calc-row">
-                    <button className="calc-button width-2">0</button>
-                    <button className="calc-button">.</button>
-                    <button className="calc-button calc-button-op">=</button>
-                </div>
-            </div>
+  const handleClickOperator = (event, operator) => {
+    resetButtons();
+    switch (currentOperator) {
+      case '+':
+        setDisplayedNum(storedNum + Number(displayedNum));
+        break;
+      case '-':
+        setDisplayedNum(storedNum - Number(displayedNum));
+        break;
+      case '*':
+        setDisplayedNum(storedNum * Number(displayedNum));
+        break;
+      case '/':
+        setDisplayedNum(storedNum / Number(displayedNum));
+        break;
+    }
+    setCurrentOperator(operator);
+    setCalculateMode(true);
+    if (operator !== '=') {
+      event.target.classList.add('calc-button-op-selected');
+    }
+  };
+
+  const resetButtons = () => {
+    if (document.querySelector('.calc-button-op-selected')) {
+      document.querySelector('.calc-button-op-selected').classList.remove('calc-button-op-selected');
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1>React Calculator</h1>
+      <div className="calc-container">
+        <p>Values: </p>
+        <div className="answer-box">{displayedNum}</div>
+        <div className="calc-row">
+          <button
+            className="calc-button calc-button-top"
+            onClick={() => {
+              setDisplayedNum('0');
+              setCurrentOperator('');
+              setStoredNum(0);
+              setCalculateMode(false);
+              resetButtons();
+            }}
+          >
+            AC
+          </button>
+          <button className="calc-button calc-button-top">+/-</button>
+          <button className="calc-button calc-button-top">%</button>
+          <button className="calc-button calc-button-op" onClick={(event) => handleClickOperator(event, '/')}>
+            /
+          </button>
         </div>
-    )
-}
+        <div className="calc-row">
+          <button className="calc-button" onClick={() => handleClickNum('7')}>
+            7
+          </button>
+          <button className="calc-button" onClick={() => handleClickNum('8')}>
+            8
+          </button>
+          <button className="calc-button" onClick={() => handleClickNum('9')}>
+            9
+          </button>
+          <button className="calc-button calc-button-op" onClick={(event) => handleClickOperator(event, '*')}>
+            x
+          </button>
+        </div>
+        <div className="calc-row">
+          <button className="calc-button" onClick={() => handleClickNum('4')}>
+            4
+          </button>
+          <button className="calc-button" onClick={() => handleClickNum('5')}>
+            5
+          </button>
+          <button className="calc-button" onClick={() => handleClickNum('6')}>
+            6
+          </button>
+          <button className="calc-button calc-button-op" onClick={(event) => handleClickOperator(event, '-')}>
+            -
+          </button>
+        </div>
+        <div className="calc-row">
+          <button className="calc-button" onClick={() => handleClickNum('1')}>
+            1
+          </button>
+          <button className="calc-button" onClick={() => handleClickNum('2')}>
+            2
+          </button>
+          <button className="calc-button" onClick={() => handleClickNum('3')}>
+            3
+          </button>
+          <button className="calc-button calc-button-op" onClick={(event) => handleClickOperator(event, '+')}>
+            +
+          </button>
+        </div>
+        <div className="calc-row">
+          <button className="calc-button width-2" onClick={() => handleClickNum('0')}>
+            0
+          </button>
+          <button className="calc-button" onClick={() => handleClickNum('.')}>
+            .
+          </button>
+          <button className="calc-button calc-button-op" onClick={(event) => handleClickOperator(event, '=')}>
+            =
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Calculator
+export default Calculator;
